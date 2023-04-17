eda779844a7942b1b4fbaa5afcb537485d09ba93
kshwi <kshwiss@gmail.com>
2023-04-16 22:44:19 -0500

    added some javescript and got help from classmates

--- index.js
+++ index.js
@@ -1,90 +1,41 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      message: "What color would you like the logo to be?",
-      name: "color"
-    },
-    {
-      type: "input",
-      message: "What shape would you like the logo to be?",
-      name: "shape"
-    },
-    {
-      type: "input",
-      message: "What 3 letters would you like displayed in the logo?",
-      name: "letters"
-    }
-  ]);
-};
-const generateLOGO = ({ color, shape, letters }) =>
-  `## Title
-  
-    This project is titled ${Title}
-  
-    ${GM.renderLicenseBadge(License)}
-    
-    ## Description
-  
-    ${Description}
-  
-    ## Table of Contents
-  
-    [Go to Installation section](#installation)
-  
-    [Go to Usage section](#usage)
-  
-    [Go to Credits section](#credits)
-  
-    [Go to License section](#license)
-  
-    [Go to Tests section](#tests)
-  
-    [Go to Questions section](#questions)
-    
-  ## Installation
-  
-  ${Installation}
-  
-  ## Usage
-  
-  ${Usage}
-  
-  ## Credits
-  
-  ${Contributing}
-  
-  ## License
-  
-  ${License}
-  
-  ${GM.renderLicenseLink(License)}
-  
-  
-  ## Tests 
-  
-  ${Tests}
-  
-  ## Questions
-  
-  You can reach me at ${Email} with any additional questions.
-  
-  My Github profile is named ${Github}, reachable at https://www.github.com/${Github}`;
+const promptUser = [
+  {
+    type: "input",
+    message: "What color would you like the logo to be?",
+    name: "Scolor"
+  },
+  {
+    type: "list",
+    message: "What shape would you like the logo to be?",
+    name: "shape",
+    choices: ["Square", "Triangle", "Circle"]
+  },
+  {
+    type: "input",
+    message: "What 3 letters would you like displayed in the logo?",
+    name: "letters"
+  },
+  {
+    type: "input",
+    message: "What color would you like the logo text to be?",
+    name: "Tcolor"
+  }
+];
 
 // TODO: Create a function to initialize app
 const init = () => {
   promptUser()
     .then(answers =>
-      fs.writeFile("README.md", generateREADME(answers), err => {
+      fs.writeFile("logo.svg", generateREADME(answers), err => {
         if (err) {
           console.log(err);
         }
       })
     )
-    .then(() => console.log("Successfully wrote to README.md"))
+    .then(() => console.log("Successfully wrote to logo.svg"))
     .catch(err => console.error(err));
 };
 
