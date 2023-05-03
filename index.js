e433cadae95bd362fcfd70e45fec6e04000aca64
kshwi <kshwiss@gmail.com>
2023-05-03 18:24:49 -0500

    changed the shapes page and the switch function

--- index.js
+++ index.js
@@ -1,38 +1,69 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const SuperSVG = require("@svgdotjs/svg.js");
-const exp = require("express");
-const app = express();
-const svg = SVG();
-const prompts = [
-  {
-    type: "input",
-    message: "What color would you like the logo to be?",
-    name: "Scolor"
-  },
-  {
-    type: "list",
-    message: "What shape would you like the logo to be?",
-    name: "shape",
-    choices: ["Square", "Triangle", "Circle"]
-  },
-  {
-    type: "input",
-    message: "What 3 letters would you like displayed in the logo?",
-    name: "letters"
-  },
-  {
-    type: "input",
-    message: "What color would you like the logo text to be?",
-    name: "Tcolor"
-  }
-];
+const { Triangle, Circle, Square } = require("./lib/shapes.js");
+const SH = require("./lib/shapes.js");
 
-// const generateSHAPE = ({
-//   Scolor,
-//   shape,
-//   letters,
-//   Tcolor
-// }) =>
+// Things to do: define a class that has a constructor with 3 methods. render, set text element, set shape inside the svg string, new shape. then use the write to file to write the data, answers, generate svg file+logo
+class Something {
+  constructor() {}
+}
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "What color would you like the logo to be?",
+      name: "Scolor"
+    },
+    {
+      type: "list",
+      message: "What shape would you like the logo to be?",
+      name: "shape",
+      choices: ["Square", "Triangle", "Circle"]
+    },
+    {
+      type: "input",
+      message: "What 3 letters would you like displayed in the logo?",
+      name: "text"
+    },
+    {
+      type: "input",
+      message: "What color would you like the logo text to be?",
+      name: "Tcolor"
+    }
+  ]);
+};
 
-fs.writeFileSync("logo.svg", svg.svg());
+const generateLogo = ({ answers.Scolor, answers.shape, answers.text, answers.Tcolor }) =>
+  SH.Shapeshifter(shape);
+
+const init = () => {
+  promptUser()
+    .then(answers => {
+      console.log(answers);
+      fs.writeFile("Logo.svg", generateLogo(answers), err => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    })
+    .then(() => console.log("Successfully wrote to logo.svg"))
+    .catch(err => console.error(err));
+};
+
+// const generateLogo = ({ Scolor, shape, letters, Tcolor }) => {
+//   promptUser()
+//     .then(answers =>
+//       fs.writeFile("Logo.svg", generateLogo(answers), err => {
+//         if (err) {
+//           console.log(err);
+//         }
+//       })
+//     )
+//     .then(() => console.log("Successfully wrote to README.md"))
+//     .catch(err => console.error(err));
+// };
+
+// Function call to initialize app
+init();
+
+// fs.writeFileSync("logo.svg", svg.svg());
